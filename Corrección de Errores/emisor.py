85ccd734da4fb77be8add093a9ff75277292be9f
Mark Albrand <mark.albrand5@gmail.com>
2024-07-21 13:46:29 -0600

    Hamming Emisor
    Co-Authored-By: Jimena Hernández <77686378+Kojimena@users.noreply.github.com>

--- Corrección de Errores/emisor.py
+++ Corrección de Errores/emisor.py
@@ -1,17 +1,24 @@
 def hamming(message, m, n):
-    print("Mensaje original: ", message)
-    print("Número de bits de datos: ", m)
-    print("Número de bits de paridad: ", n)
+    """
+    Función hamming(m, n) que recibe un mensaje de m bits y añade n bits de paridad
+    :param message: Mensaje binario
+    :param m: Número de bits del mensaje
+    :param n: Número de bits de paridad
+    :return: Mensaje con bits de paridad
+    """
+    
+    print("\n - Mensaje original: ", message)
+    print("\n -Número de bits en la cadena: ", m)
+    print("\n -Número de bits de paridad: ", n)
 
-    list_bits = list(message)
-    list_bits = list_bits[::-1]
+    list_bits = list(message) # Convertimos el mensaje en una lista de bits
+    list_bits = list_bits[::-1 ] 
 
     for i in range(n):  # Iteramos sobre los bits de paridad
         pos = 2**i
-        
         list_bits.insert(pos - 1, 'p')
 
-    print("Mensaje con bits de paridad: ", ''.join(list_bits[::-1]))
+    print("\n Mensaje con bits de paridad: ", ''.join(list_bits[::-1]))
 
     for i in range(n):  # Iteramos sobre los bits de paridad
         pos = 2**i
@@ -19,14 +26,16 @@ def hamming(message, m, n):
 
         for j in range(1, len(list_bits) + 1):
             if j & pos == pos and list_bits[j - 1] == '1':
+                # Si el bit j tiene el bit i-ésimo activado (pos) y es 1
                 count += 1
 
+        # Asignación del bit de paridad
         if count % 2 == 0:
             list_bits[pos - 1] = '0'
         else:
             list_bits[pos - 1] = '1'
 
-    print("Bits de paridad: ", ''.join(list_bits[::-1]))
+    return ''.join(list_bits[::-1])
 
 
 if __name__ == "__main__":
@@ -45,3 +54,4 @@ if __name__ == "__main__":
 
     # Calculamos los bits de paridad
     hamming = hamming(message, m, r)
+    print("\nMensaje con bits de paridad: ", hamming)
