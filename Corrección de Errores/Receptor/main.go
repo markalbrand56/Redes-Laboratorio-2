bc0faf4cbaba01d2ed9a13a15433c8a59ca1be62
Jimena Hernández <her21199@uvg.edu.gt>
2024-07-26 17:54:55 -0600

    emisor capas - corrección e

--- Corrección de Errores/Receptor/main.go
+++ Corrección de Errores/Receptor/main.go
@@ -1,14 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func startServer(port string) {
+	ln, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		fmt.Println("Error al iniciar el servidor:", err)
+		return
+	}
+	defer ln.Close()
+	fmt.Println("Servidor escuchando en el puerto", port)
+
+	for {
+		fmt.Println("Esperando conexión...")
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error al aceptar la conexión:", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
+func binaryToASCII(binary string) string {
+	ascii := ""
+	for i := 0; i < len(binary); i += 8 {
+		// Convertir el byte actual a un entero
+		byteInt := 0
+		for j := 0; j < 8; j++ {
+			bit := binary[i+j]
+			if bit == '1' {
+				byteInt += 1 << uint(7-j)
+			}
+		}
+		// Convertir el entero a un caracter ASCII
+		ascii += string(byteInt)
+	}
+	return ascii
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	message, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error al leer el mensaje:", err)
+		return
+	}
+	message = message[:len(message)-1] // Elimina el salto de línea
+	fmt.Printf("Mensaje recibido: %s\n", message)
+	decodedMessage := HammingDecode(message) //Capa de enlace
+	fmt.Printf("Mensaje decodificado: %s\n", decodedMessage)
+	//decode in ascii
+	decodedMessage = binaryToASCII(decodedMessage)
+	fmt.Printf("Mensaje decodificado en ASCII: %s\n", decodedMessage)
+}
 
 func main() {
 
-	fmt.Println("Ingrese un mensaje en binario:")
+	fmt.Println("Ingrese el puerto para el servidor:")
+	var port string
+	fmt.Scanln(&port)
+
+	// Iniciar el servidor
+	go startServer(port)
+
+	// Mantener el programa principal en ejecución
+	fmt.Println("Presione 'Enter' para terminar...")
+	fmt.Scanln()
+
+	/* fmt.Println("Ingrese un mensaje en binario:")
 	var message string
 	fmt.Scanln(&message)
 
 	res := HammingDecode(message)
 
-	fmt.Println("Resultado: ", res)
+	fmt.Println("Resultado: ", res) */
 }
